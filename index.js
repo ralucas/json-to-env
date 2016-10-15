74bd6416ffc4724b15472bb59bd8f0f46f17d032
R.A. Lucas <richard@richardalucas.com>
2016-10-15 13:05:55 -0600

    updating to cli utility

---
+++ index.js
@@ -0,0 +1,114 @@
+var jsonToEnv = require('./lib');
+var pkg = require('./package.json');
+
+var args = process.argv.slice(2);
+
+var OPTIONS = {
+  version: {
+    commands: ['--version', '-V'],
+    text: 'Returns the version',
+    value: function() {
+      process.stdout.write('Version: ' + pkg.version + '\n');
+      process.exit(0);
+    }
+  },
+  help: {
+    commands: ['--help', '-h'],
+    text: 'Returns how to use json-to-env',
+    value: function() {
+      return printUsage();
+    }
+  },
+  verbose: {
+    commands: ['--verbose', '-v'],
+    text: 'Runs it in verbose'
+  }
+};
+
+function printUsage() {
+  var options = OPTIONS;
+  var usageString = 'Using json-to-env\n\n\t' +
+    'json-to-env <inputfile.json> <outputfile.config> <options>\n\n';
+  for (var key in options) {
+    usageString += key + '\n\t' + 
+      options[key].commands.join(', ') + '\n\t' +
+      options[key].text + '\n';
+  }
+  process.stdout.write(usageString);
+  process.exit(0);
+}
+
+function getOptions(optionArr, optionalCmds) {
+  // Check for any options
+  var options = OPTIONS;
+  var opts = optionArr.filter(function(arg) {
+    return ~optionalCmds.indexOf(arg);
+  }).map(function(o) {
+    var obj = {};
+    if ( o ) {
+      for (var m in options) {
+        if ( ~options[m].commands.indexOf(o) ) {
+          obj[m] = options[m];
+          return obj;
+        }
+      } 
+    }      
+  });
+  // Run any functions from options here and exit
+  opts.forEach(function(cmd) {
+    if ( cmd ) {
+      for (var k in cmd) {
+        if ( cmd[k].value ) {
+          cmd[k].value();
+        }
+      }
+    }
+  });
+
+  return opts;
+}
+
+function getOptionsCommands(opts) {
+  var optionalCmds = [];
+  for (var k in opts) {
+    optionalCmds = optionalCmds.concat(opts[k].commands);
+  }
+
+  return optionalCmds;
+}
+
+function parseArgs(args) {
+  var out = {};
+
+  var optCmds = getOptionsCommands(OPTIONS);
+
+  if ( (/^[\-]/).test(args[0]) ) {
+    process.stderr.write('Please put option arguments at the end\n' + printUsage());
+  }
+
+  out.input = args.shift();
+  out.output = args.shift();
+
+  opts = getOptions(args, optCmds);
+
+  if ( opts.length ) {
+    opts.forEach(function(opt) {
+      var key = Object.keys(opt);
+      out[key[0]] = opt;
+    });
+  }
+  return out; 
+}
+if ( !args.length ) {
+  return process.stdout.write(printUsage());
+}
+
+var config = parseArgs(args);
+
+if (config.verbose) {
+  process.stdout.write('Running verbose mode\nStarting...\n');
+}
+
+module.exports = jsonToEnv(config);
+
+
